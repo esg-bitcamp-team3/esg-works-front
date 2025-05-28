954c1953d085608933e9c07a76082e1d17686591
gnuoy-ji <qkrwldud0603@gmail.com>
2025-05-28 19:52:36 +0900

    Add chartp-modal-1(select, checkBox, tag)

--- src/app/(main)/report/page.tsx
+++ src/app/(main)/report/page.tsx
@@ -1,8 +1,14 @@
+import ChartModal from "@/components/modal/chart-modal";
+import ReportModal from "@/components/modal/document-modal";
+
 const Page = () => {
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Main Page</h1>
       <p className="text-lg">This is the main content area.</p>
+      <ChartModal/>
+      {/* <ReportModal /> */}
     </div>
   );
 };
