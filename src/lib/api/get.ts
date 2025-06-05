29ea9f2f8a450b5bb274064eb2c39cc07e6523a5
hyosikKim98 <gytlr339@gmail.com>
2025-06-05 09:38:27 +0900

    asdf

--- src/lib/api/get.ts
+++ src/lib/api/get.ts
@@ -1,10 +1,4 @@
-import {
-  Category,
-  CategoryList,
-  DataFilter,
-  PartialESGData,
-  Section,
-} from "../interface";
+import { Category, DataFilter, PartialESGData, Section } from "../interface";
 import { apiClient } from "./client";
 import { ESGData } from "./interfaces/esgData";
 export async function getSearchSectionId(sectionId: string) {
@@ -27,13 +21,8 @@ export async function getDataByCorpYear(data: DataFilter) {
     return response.data;
   } catch (error) {}
 }
-import { apiClient } from "./client";
-<<<<<<< HEAD
 import { ChartDetail } from "./interfaces/chart";
-=======
 import { CategorizedESGDataList } from "./interfaces/categorizedEsgDataList";
-import { CategoryDetail, Section } from "./interfaces/categoryDetail";
->>>>>>> bafd3eb8ca00f6494d4544c8768f7f58352c6529
 
 export const getSections = async () => {
   try {
@@ -46,7 +35,6 @@ export const getSections = async () => {
 };
 
 export const getCategories = async (sectionId: string) => {
-<<<<<<< HEAD
   try {
     const res = await apiClient.get<Category[]>(
       `/categories/by-section/${sectionId}`
@@ -58,17 +46,17 @@ export const getCategories = async (sectionId: string) => {
   }
 };
 
-export const getEsgData = async (categoryId: string, selected: string[]) => {
-  try {
-    const res = await apiClient.get<ESGData[]>(
-      `/esg-data/category/${categoryId}`
-    );
-    return res.data;
-  } catch (error) {
-    console.error("ESG 데이터 가져오기 실패:", error);
-    return null;
-  }
-};
+// export const getEsgData = async (categoryId: string, selected: string[]) => {
+//   try {
+//     const res = await apiClient.get<ESGData[]>(
+//       `/esg-data/category/${categoryId}`
+//     );
+//     return res.data;
+//   } catch (error) {
+//     console.error("ESG 데이터 가져오기 실패:", error);
+//     return null;
+//   }
+// };
 
 export const getChart = async () => {
   try {
@@ -80,25 +68,24 @@ export const getChart = async () => {
     return null;
   }
 };
-=======
-    try {
-        const res = await apiClient.get<CategoryDetail[]>(`/categories/by-section/${sectionId}`);
-        return res.data;
-    } catch (error) {
-        console.error("카테고리 가져오기 실패:", error);
-        return [];
-    }
-}
+//     try {
+//         const res = await apiClient.get<CategoryDetail[]>(`/categories/by-section/${sectionId}`);
+//         return res.data;
+//     } catch (error) {
+//         console.error("카테고리 가져오기 실패:", error);
+//         return [];
+//     }
+// }
 
 export const getEsgData = async (categoryId: string, selected: string[]) => {
-    try {
-        const res = await apiClient.get<CategorizedESGDataList[]>(`/esg-data/category/${categoryId}`);
-        console.log("보내는 categoryId:", categoryId);
-        return res.data;
-    } catch (error) {
-        console.error("ESG 데이터 가져오기 실패:", error);
-        return null;
-    }
-}
-
->>>>>>> bafd3eb8ca00f6494d4544c8768f7f58352c6529
+  try {
+    const res = await apiClient.get<CategorizedESGDataList[]>(
+      `/esg-data/category/${categoryId}`
+    );
+    console.log("보내는 categoryId:", categoryId);
+    return res.data;
+  } catch (error) {
+    console.error("ESG 데이터 가져오기 실패:", error);
+    return null;
+  }
+};
