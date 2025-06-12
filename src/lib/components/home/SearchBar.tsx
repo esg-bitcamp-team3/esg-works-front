09f95de2657fbcbd8545f8dbab49e9e3c727c9ae
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-06-12 15:59:27 +0900

    connect back and front for report

--- src/lib/components/home/SearchBar.tsx
+++ src/lib/components/home/SearchBar.tsx
@@ -1,21 +1,3 @@
-// export default function SearchBar({
-//   keyword,
-//   setKeyword,
-// }: {
-//   keyword: string;
-//   setKeyword: (v: string) => void;
-// }) {
-//   return (
-//     <input
-//       type="text"
-//       value={keyword}
-//       onChange={(e) => setKeyword(e.target.value)}
-//       placeholder="검색"
-//       style={{ borderRadius: "10px", padding: "0.5rem", width: "300px" }}
-//     />
-//   );
-// }
-
 import { Input, InputGroup, Box } from "@chakra-ui/react";
 import { TbSearch } from "react-icons/tb";
 import React from "react";
@@ -34,26 +16,3 @@ const SearchBar = ({ keyword, setKeyword }: SearchBarProps) => (
   />
 );
 export default SearchBar;
-
-// export default function SearchBar({ keyword, setKeyword }: SearchBarProps) {
-//   return (
-//     <InputGroup
-//       startElement={
-//         <Box pl="3" display="flex" alignItems="center">
-//           <TbSearch />
-//         </Box>
-//       }
-//       alignItems="start"
-//       w="2xl"
-//     >
-//       <Input
-//         placeholder="검색"
-//         borderRadius="2xl"
-//         size="lg"
-//         p="2"
-//         value={keyword}
-//         onChange={(e) => setKeyword(e.target.value)}
-//       />
-//     </InputGroup>
-//   );
-// }
